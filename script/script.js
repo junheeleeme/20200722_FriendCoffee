be3a6f9f70facb3e7d11b24464a70c10e8bd1cae
kimeb2020 <kimeb2020@gmail.com>
2020-07-24 10:31:44 +0900

    2020-07-24

---
+++ script/script.js
@@ -0,0 +1,9 @@
+
+
+function goModal() {    // 사용자정의 함수명
+    document.getElementById("modalArea").style.display="block";
+}
+
+function popUpClose() {
+    document.getElementById("modalArea").style.display="none";
+}
